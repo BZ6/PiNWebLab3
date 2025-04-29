9681546180f139f413481b4b59c5e9478c5509af
Bogdan Zaharkin <bogdanzaharkin8@gmail.com>
2025-04-29 16:27:35 +0300

    Add celery and redis

--- Task12/sheduler/src/api/parser/routes.py
+++ Task12/sheduler/src/api/parser/routes.py
@@ -1,16 +1,27 @@
 from fastapi import APIRouter, HTTPException, status
-import requests
+from celery.result import AsyncResult
+from asyncio import sleep
+
+from src.task import parse_url
 
 
 router = APIRouter(prefix="/parser", tags=["parser"])
 
 
 @router.post("/")
-def parse_site(url: str):
+async def parse_site(url: str):
     try:
-        response = requests.post("http://parser:8000/parse",
-                                 json={"url": url})
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
+        task = parse_url.apply_async((url,))
+        result = AsyncResult(task.id)
+
+        # Не хочу писать еще один эндпоинт который показывает статус
+        # таски, поэтому пусть таска поспит пока не подойдет ее очередь
+        while not result.ready():
+            await sleep(1)
+
+        if not result.successful():
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=result.result)
+        
+        return result.result
+    except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
